49d7939a427dfe70c676398ed40d67e1d07b0358
Nithin R <37147336+thebinarybot@users.noreply.github.com>
2020-08-16 22:35:15 +0530

    Update __init__.py
    Initial upload getting requests from muffinlabs/date api and added it to custom diaglog

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import requests
 
 class Todayinhistory(MycroftSkill):
     def __init__(self):
@@ -7,7 +7,23 @@ class Todayinhistory(MycroftSkill):
 
     @intent_file_handler('todayinhistory.intent')
     def handle_todayinhistory(self, message):
-        self.speak_dialog('todayinhistory')
+        url = "https://history.muffinlabs.com/date"
+        r = requests.get(url)
+        #print(r.text)
+        json_op = r.json()
+        #print("Json Output : {} ".format(json_op))
+        '''print(json_op.keys())
+        print(json_op['date'])
+        print(json_op['url'])
+        print(json_op['data'])'''
+        op = json_op['data']
+        events = op['Events']
+        births = op['Births']
+        deaths = op['Deaths']
+        #print(events[0])
+        todays_event = events[0]['text']
+        #print(todays_event)
+        self.speak_dialog('Today in History {}'.format(events[0]['text']))
 
 
 def create_skill():
